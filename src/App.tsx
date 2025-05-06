e84fb68a1b8b7417ee2b03446d1fd463ed720acb
GabrielDani <gabriel.dani07@gmail.com>
2025-05-05 22:34:24 -0300

    initial vite with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-lg bg-gray-400 text-yellow-400 hover:text-green-300">
+        Olá mundo!
+      </h1>
+    </>
+  );
+}
+
+export default App;
