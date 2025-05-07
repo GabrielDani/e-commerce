4b0388728017452a68c963dc1e32bead71da7f3e
GabrielDani <gabriel.dani07@gmail.com>
2025-05-07 14:08:47 -0300

    changing z header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import { HeaderLogo } from "../molecules/HeaderLogo";
 
 const Header = () => {
   return (
-    <header className="w-full bg-background-header dark:bg-background-header-dark shadow-md sticky top-0 z-50">
+    <header className="w-full bg-background-header dark:bg-background-header-dark shadow-md sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <HeaderLogo />
         <HeaderNavLinks />
